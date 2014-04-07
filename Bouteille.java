4b0aafc62b4539cdadcc4cb37c0fd3da7ea81842
zazou001 <zachary.zanzi@gmail.com>
2014-04-07 13:04:22 +0200

    Update Bouteille.java

--- Bouteille.java
+++ Bouteille.java
@@ -1,7 +1,27 @@
 public class Bouteille extends Breuvage {
 	int taille;
-	String type;
-	String Bouchon;
+	String format;
+	String bouchon;
 	
+	public Bouteille(int size, String type, String capsule)
+	{
+		taille=size;
+		format=type;
+		Bouchon=capsule;
+	}
 	
+	public String getTaille()
+	{
+		return taille;
+	}
+	
+	public String getFormat()
+	{
+		return format;
+	}
+	
+	public String getBouchon()
+	{
+		return bouchon;
+	}
 }
