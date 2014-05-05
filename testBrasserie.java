15289d9cc4132e19227773c5ca47d0a0a866b15f
Stygmates <styg.tan.dat@gmail.com>
2014-05-05 17:03:58 +0200

    Fonctions de test de la classe brasserie terminee.

--- testBrasserie.java
+++ testBrasserie.java
@@ -6,6 +6,12 @@ public class testBrasserie{
 		b2 = new Brasserie("Heineken","Dusseldorf","Pays-Bas");
 		System.out.println(b1);
 		System.out.println(b2);
+		b2.setNom("Schiltigheim");
+		System.out.println(b2);
+		b1.setVille("Paris");
+		System.out.println(b1);
+		b1.setPays("Allemagne");
+		System.out.println(b1);
 	}
 }
 		
