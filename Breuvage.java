33d2e549774a257473f9753dba41cb57bb56ffa3
Stygmates <styg.tan.dat@gmail.com>
2014-05-05 17:20:08 +0200

    Ajout des commentaires.

--- Breuvage.java
+++ Breuvage.java
@@ -1,46 +1,79 @@
 public class Breuvage {
-	private String fermentation;/*Type de fermentation*/
+	private String typefermentation;/*Type de fermentation*/
 	private String particularite;/*Fermentation*/
   private String couleur;/*Couleur du breuvage*/
    
-  public Breuvage(String type, String spec)
+   /**
+    * Constructeur de breuvage.
+    * @param type Le type de fermentation : basse/haute/sur bouteille/sur lie...
+    * @param spec La fermentation : simple/doublie/triple...
+    * @param couleur La couleur du breuvage.
+    * */
+  public Breuvage(String type, String spec,String couleur)
   {
-		this.fermentation = type;
+		this.typefermentation = type;
     this.particularite = spec;
+    this.couleur = couleur;
   }
   
-  public void setFermentation(String fermentation)
+	/**
+		* Modifie le type de fermentation : basse/haute/sur bouteille/sur lie...
+		* @param fermentation Le nouveau nom du champ fermentation.
+		* */  
+  public void setTypeFermentation(String fermentation)
   {
-		this.fermentation = fermentation;
+		this.typefermentation = fermentation;
 	}
 	
+	/**
+	 * Modifie le champ fermentation :simple/double/triple...
+	 * @param particularite La nouvelle fermentation.
+	 * */
 	public void setParticularite(String particularite)
 	{
 		this.particularite=particularite;
 	}
 	
+	/**
+	 * Modifie le champ couleur du breuvage.
+	 * @param couleur La nouvelle couleur.
+	 * */
 	public void setCouleur(String couleur)
 	{
 		this.couleur = couleur;
 	}
   
-  public String getFermentation()
+  /**
+   * Retourne le champ fermentation.
+   * @return Le champ fermentation du breuvage.
+   * */
+  public String gettypeFermentation()
 	{
-		return this.fermentation;
+		return this.typefermentation;
 	}
-	   
+	
+	/**
+	 * Retourne la fermentation : simple/double...
+	 * @return La fermentation.
+	 * */
   public String getParticularite()
 	{
 		return this.particularite;
 	}
-	
+	/**
+	 * Retourne la couleur du breuvage.
+	 * @return La couleur du breuvage.
+	 * */
 	public String getCouleur()
 	{
 		return this.couleur;
 	}
 	
+	/**
+	 * Redéfinition de la methode toString.
+	 * */
 	public String toString()
 	{
-		return "la fermentation est " + this.getFermentation() + ", a pour particularité " + this.getParticularite() + " et est " + this.getCouleur() + ".";
+		return "la fermentation est " + this.gettypeFermentation() + ", a pour particularité " + this.getParticularite() + " et est " + this.getCouleur() + ".";
 	}
 }
