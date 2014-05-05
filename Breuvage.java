d6aa72fe1a0a6f9091ce70965d10fa307d066087
zazou001 <zachary.zanzi@gmail.com>
2014-05-05 16:20:00 +0200

    creation de toString()

--- Breuvage.java
+++ Breuvage.java
@@ -1,12 +1,25 @@
 public class Breuvage {
-        String fermentation;
-        String particularite;
+	String fermentation;
+    String particularite;
         
-        public Breuvage(String type, String spec)
-        {
-                fermentation=type;
-                particularite=spec;
-        }
+    public Breuvage(String type, String spec)
+    {
+		fermentation=type;
+        particularite=spec;
+    }
         
-        
+    public String getFermentation()
+	{
+		return fermentation;
+	}
+	   
+	    public String getParticularite)
+	{
+		return particularite;
+	}
+	
+	public String toString()
+	{
+		return "la fermentation est " + this.getFermentation() + ", a pour particularité" + this.getParticularite();
+	}
 }
