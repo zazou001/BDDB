13ac7bd6e55269070542f83abf1757d746cde507
zazou001 <zachary.zanzi@gmail.com>
2014-05-05 15:26:46 +0200

    Create biere.java

---
+++ biere.java
@@ -0,0 +1,23 @@
+public class Biere {
+	private int idBiere;
+	private String nom;
+	private int alcool;
+	private int idBouteille;
+	private int idBrasserie;
+	private int idBrevage;
+
+	public Biere(int biere, String name, String degree,int bout, int Bras, int Bre )
+	{
+
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public int getAlcool()
+	{
+		return alcool;
+	}
+}
