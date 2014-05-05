9ff0a21ef2ebdaa484f77f84e58693df596f9396
Stygmates <styg.tan.dat@gmail.com>
2014-05-05 16:08:50 +0200

    Ajout de la classe de test de Brasserie.

---
+++ testBrasserie.java
@@ -0,0 +1,11 @@
+public class testBrasserie{
+	public static void main(String args[])
+	{
+		Brasserie b1,b2;
+		b1 = new Brasserie("Kronenbourg","Strasbourg","France");
+		b2 = new Brasserie("Heineken","Dusseldorf","Pays-Bas");
+		System.out.println(b1);
+		System.out.println(b2);
+	}
+}
+		
