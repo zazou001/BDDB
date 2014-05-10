62dbde6ee682962532846360e35b1b9156345d15
zazou001 <zachary.zanzi@gmail.com>
2014-05-10 15:58:41 +0200

    Affichage graphique du contenue de la base de donnée

--- BDDB.java
+++ BDDB.java
@@ -1,4 +1,4 @@
-//import pkg.affichage.*;
+import pkg.affichage.*;
 import pkg.donnee.*;
 import pkg.connection.*;
 import pkg.dao.*;
@@ -9,20 +9,14 @@ public class BDDB{
 	
 	public static void main(String[] args)
 	{
+		
         Connexion connexion = new Connexion("biere.bd");
         Connection connect = connexion.connect();
-		DAO<Breuvage> breuvageDao = new BreuvageDAO(connect);
-		DAO<Bouteille> bouteilleDao = new BouteilleDAO(connect);
-		DAO<Brasserie> brasserieDao = new BrasserieDAO(connect);
-		Bouteille bouteille = new Bouteille(1,33,"normal","capsule");
-		bouteilleDao.create(bouteille);
-		Bouteille b1 = bouteilleDao.find(1);
-		bouteilleDao.delete(b1);
-        Breuvage breuvage = breuvageDao.find(2);
-		System.out.println("Biere N°" +breuvage.getTypeFermentation() +" - " + breuvage.getParticularite() + " " + breuvage.getAlcool());
+        ModeleObjet modele = new ModeleObjet(connect);
 
+		new Fenetre(modele).setVisible(true);
         connexion.close();	
-		//new Fenetre().setVisible(true);
+		//
 	}
 }
 		
