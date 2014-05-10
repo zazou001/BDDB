d549c18b60f98a5716cb2ce745220e5fa15b6407
zazou001 <zachary.zanzi@gmail.com>
2014-05-10 12:22:15 +0200

    Test d'ajout et de suppression dans la BDD

--- BDDB.java
+++ BDDB.java
@@ -3,14 +3,23 @@ import pkg.donnee.*;
 import pkg.connection.*;
 import pkg.dao.*;
 
+import java.sql.Connection;
+
 public class BDDB{
 	
 	public static void main(String[] args)
 	{
         Connexion connexion = new Connexion("biere.bd");
-		DAO<Breuvage> breuvageDao = new BreuvageDAO(connexion.connect());
+        Connection connect = connexion.connect();
+		DAO<Breuvage> breuvageDao = new BreuvageDAO(connect);
+		DAO<Bouteille> bouteilleDao = new BouteilleDAO(connect);
+		DAO<Brasserie> brasserieDao = new BrasserieDAO(connect);
+		Bouteille bouteille = new Bouteille(1,33,"normal","capsule");
+		bouteilleDao.create(bouteille);
+		Bouteille b1 = bouteilleDao.find(1);
+		bouteilleDao.delete(b1);
         Breuvage breuvage = breuvageDao.find(2);
-		System.out.println("Biere N°" +breuvage.getId() +" - " + breuvage.getNom() + " " + breuvage.getAlcool());
+		System.out.println("Biere N°" +breuvage.getTypeFermentation() +" - " + breuvage.getParticularite() + " " + breuvage.getAlcool());
 
         connexion.close();	
 		//new Fenetre().setVisible(true);
