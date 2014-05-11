c0a69351b796f7796e703975e2ad6326c1ce01a6
zazou001 <zachary.zanzi@gmail.com>
2014-05-11 18:56:01 +0200

    Base de Donnée

--- biere.sql
+++ biere.sql
@@ -1,4 +1,4 @@
-CREATE TABLE bouteille 
+CREATE TABLE Bouteille 
 (
 	idBouteille integer primary key, 
 	taille integer not null, 
@@ -6,7 +6,7 @@ CREATE TABLE bouteille
 	bouchon VARCHAR(30) not null
 );
 
-CREATE TABLE brasserie
+CREATE TABLE Brasserie
 (
 	idBrasserie integer primary key,
 	nomBrasserie VARCHAR(50) not null,
@@ -14,7 +14,7 @@ CREATE TABLE brasserie
 	pays VARCHAR(30)
 );
 
-CREATE TABLE breuvage
+CREATE TABLE Breuvage
 (
 	idBreuvage integer primary key,
 	nom VARCHAR(20) not null,
@@ -25,35 +25,35 @@ CREATE TABLE breuvage
 	annee integer,
 	note integer,
 	commentaire VARCHAR(146),
-	idBouteille integer references bouteille(id),
-	idBrasserie integer not null references brasserie(id)
+	idBouteille integer references bouteille(idBouteille),
+	idBrasserie integer not null references brasserie(idBrasserie)
 );
 
-create view biere as
-	select breuvage.*, brasserie.nomBrasserie, brasserie.ville,  brasserie.pays, bouteille.taille, bouteille.format, bouteille.bouchon
-		from breuvage, brasserie, bouteille 
-	where (breuvage.idBrasserie = brasserie.idBrasserie) 
-		AND (breuvage.idBouteille = bouteille.idBouteille);
+create view Biere as
+	select Breuvage.*, Brasserie.nomBrasserie, Brasserie.ville,  Brasserie.pays, Bouteille.taille, Bouteille.format, Bouteille.bouchon
+		from Breuvage, Brasserie, Bouteille 
+	where (Breuvage.idBrasserie = Brasserie.idBrasserie) 
+		AND (Breuvage.idBouteille = Bouteille.idBouteille);
 
-insert into "breuvage" values(1, "Kronenbourg",5,null, null,"Blonde", null, null, null, 1, 1);
-insert into "brasserie" values(1,"Brasseries Kronenbourg", "Strasbourg", "France");
-insert into "bouteille" values(1,33,"normal","capsule");
+insert into "Breuvage" values(1, "Kronenbourg",5,null, null,"Blonde", null, null, null, 1, 1);
+insert into "Brasserie" values(1,"Brasseries Kronenbourg", "Strasbourg", "France");
+insert into "Bouteille" values(1,33,"normal","capsule");
 
-insert into "breuvage" values(2, "Scotch Ale",5.8,null, null,"Ambrée", 2005, null, null, 1, 2);
-insert into "brasserie" values(2,"Red Hill Brewery", null, "Australie");
-insert into "bouteille" values(2,50,"normal","capsule");
+insert into "Breuvage" values(2, "Scotch Ale",5.8,null, null,"Ambrée", 2005, null, null, 1, 2);
+insert into "Brasserie" values(2,"Red Hill Brewery", null, "Australie");
+insert into "Bouteille" values(2,50,"normal","capsule");
 
-insert into "breuvage" values(3, "La chouffe",8,null, null,"Blonde", 1982, null, null, 1, 3);
-insert into "brasserie" values(4,"Brasserie D'Aschouffe", null, "Belgique");
-insert into "bouteille" values(3,75,"normal","capsule");
+insert into "Breuvage" values(3, "La chouffe",8,null, null,"Blonde", 1982, null, null, 1, 4);
+insert into "Brasserie" values(4,"Brasserie D'Aschouffe", null, "Belgique");
+insert into "Bouteille" values(3,75,"normal","capsule");
 
-insert into "breuvage" values(4, "Summer Lightning",5,null, null,"Blonde", 1988, null, null, 2, 3);
-insert into "brasserie" values(3,"Hop Back Brewery", "Salisbury", "Angleterre");
-insert into "bouteille" values(4,75,"normal","champagne");
+insert into "Breuvage" values(4, "Summer Lightning",5,null, null,"Blonde", 1988, null, null, 2, 3);
+insert into "Brasserie" values(3,"Hop Back Brewery", "Salisbury", "Angleterre");
+insert into "Bouteille" values(4,75,"normal","champagne");
 
-insert into "breuvage" values(6, "Jacobsen Sommer Wit",5.1,null, null,"Blanche", 2008, null, null, 1, 5);
-insert into "brasserie" values(5,"Husbryggeriet Jacobsen", null, "Danemark");
+insert into "Breuvage" values(6, "Jacobsen Sommer Wit",5.1,null, null,"Blanche", 2008, null, null, 1, 5);
+insert into "Brasserie" values(5,"Husbryggeriet Jacobsen", null, "Danemark");
 
-insert into "breuvage" values(5, "Kronenbourg",5,null, null,"Blonde", null, 2, "grosse biere à soif", 3, 1);
+insert into "Breuvage" values(5, "Kronenbourg",5,null, null,"Blonde", null, 2, "grosse biere à soif", 3, 1);
 
-update "breuvage" set commentaire="biere à soif" where idBreuvage =1;
+update "Breuvage" set commentaire="biere à soif" where idBreuvage =1;
